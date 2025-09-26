acdb4a305f968be84fabf036f9f4ca3cbb6d63cf
Jonathan Muhire <muhirejonathan123@gmail.com>
2025-09-25 19:34:31 -0500

    Add tutorial management system with Vue Router, Vuetify, and services

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router.js";
+import vuetify from "./plugins/vuetify.js";
 
-createApp(App).mount('#app')
+createApp(App).use(vuetify).use(router).mount("#app");
